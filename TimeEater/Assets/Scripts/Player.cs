ad5162f945fd3b0695211e6b3803881a0533ca22
Ethan Cavadia <cavadiaethan@gmail.com>
2019-01-12 16:48:33 +0100

    Auto stash before merge of "Ethan" and "origin/Adam"

---
+++ TimeEater/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] private float playerSpeed;
+
+	private Animator animator;
+	
+	void Start ()
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(Vector2.up * playerSpeed);
+		}
+
+		else if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(Vector2.left * playerSpeed);
+		}
+		
+		else if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(Vector2.down * playerSpeed);
+		}
+		
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(Vector2.right * playerSpeed);
+		}
+	}
+}
--- TimeEater/Assets/Scripts/Room1.cs
+++ TimeEater/Assets/Scripts/Room1.cs
@@ -11,29 +11,38 @@ public class Room1 : MonoBehaviour
 	[SerializeField] private GameObject candle4;
 	[SerializeField] private GameObject candle5;
 	[SerializeField] private GameObject candle6;
-
-	private bool candleStates = true;
-	private GameManager door1;
+	[SerializeField] private GameObject door1;
 	
-	void Start () 
+	void Start ()
 	{
 		
 	}
 	
 	void Update () 
 	{
-		if (Input.GetButton("Interact"))
-		{
-			
-		}
+		
 	}
 
 	private void OpenDoorCondition()
 	{
-		if ((candle2 && candle3 && candle6) == true && (candle1&&candle4&&candle5) == false)
+		if ((candle2 && candle3 && candle6).Equals(true) && (candle1&& candle4 && candle5).Equals(false))
 		{
 			door1.GetComponent<BoxCollider2D>().enabled = false;
 		}
 	}
-	
+
+	private void OnTriggerEnter(Collider candle)
+	{
+		if (candle.gameObject.CompareTag("Player"))
+		{
+			if (Input.GetButton("Interact"))
+			{
+				gameObject.GetComponent<SpriteRenderer>().enabled = false;
+			}
+			else if (Input.GetButtonDown("Interact"))
+			{
+				gameObject.GetComponent<SpriteRenderer>().enabled = true;
+			}
+		}
+	}
 }
